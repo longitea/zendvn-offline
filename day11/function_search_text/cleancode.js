26d5e7632227ff96d31b61e03c58d6ceef815bbb
longitea <longnt.en@gmail.com>
2022-06-06 16:41:54 +0700

    upload code day 12

---
+++ day11/function_search_text/cleancode.js
@@ -0,0 +1,29 @@
+var arrName = ["Minor Bisset", "Kiri Bernasek", "Malia Olenikov", "Grazia Glason", "Barbey Simion", "Dorena Wemes", "Shirlee Mc Grath", "Tull Leppo", "Mandy Jedrysik", "Dilly Sinncock", "Deena Westnedge", "Pat Wawer", "Marc Hune", "Fleming Aykroyd", "Tybalt Kaspar", "Araldo Tyson", "Sherrie Ashleigh", "Norrie Ales0", "Jarrad Prettyjohn", "Vittoria Hofer"];
+
+var listName = document.querySelector('#list-name');
+var inputSearch = document.querySelector('input');
+
+// 1. render table khi vừa vào trang
+renderTable();
+
+
+// 2. bắt sự kiện input -> render valueTable
+inputSearch.addEventListener('input', function(){
+    var search = inputSearch.value.trim();
+    renderTable(search);
+})
+
+// 3. khi vale trùng với element trong mảng thì mới render
+function renderTable(search = ''){
+    var result = '';
+    for (var element of arrName){
+        if(search !== ''){
+            if(element.includes(search)){
+                var name = element.replaceAll(search, '<mark>' + search +'</mark>')
+                result += '<tr><td>' + name + '</td></tr>';
+            }
+        }else result += '<tr><td>' + element + '</td></tr>';
+
+    }
+    listName.innerHTML = result;
+}
---
+++ day12/case02/main.js
@@ -0,0 +1,21 @@
+var clickImage = document.querySelectorAll('.box-img');
+var replaceSource = document.querySelector('.box-click img');
+var replaceClass = document.querySelector('.box-click');
+
+// 1. bắt sự kiện click vào img
+document.addEventListener('click', function(e){
+    // 1. Lấy Source IMG
+    var source = e.target.getAttribute('src');
+
+    replaceImage(source);
+    replaceClass.classList.toggle('open');
+
+})
+
+// 2. gán src của img trong box-click -> ok
+function replaceImage(newSource){
+    if(newSource !== null){
+        replaceSource.setAttribute('src', newSource);
+    }
+}
+  
